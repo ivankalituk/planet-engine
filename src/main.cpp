ecd477e92c22bef998596ec2856b898f0b34833a
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2026-09-06 02:15:48 +0300

    feat[shaders]: simple square render

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,94 @@ int main()
 
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glClearColor(0.2f, 0.4f, 0.8f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+    const char* vertexShaderSource = R"(
+        #version 330 core
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+        layout (location = 0) in vec3 aPos;
+
+        void main()
+        {
+            gl_Position = vec4(aPos, 1.0);
+        }
+    )";
+
+    const char* fragmentShaderSource = R"(
+        #version 330 core
+
+        out vec4 FragColor;
+
+        void main()
+        {
+            FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+        }
+    )";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] =
+    {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+
+         0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
+
+         0.5f,  0.5f, 0.0f,
+         -0.5f, 0.5f, 0.0f,
+
+         -0.5f, 0.5f, 0.0f,
+         -0.5f, -0.5f, 0.0f,
+    };
+
+    unsigned int VAO;
+    unsigned int VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        3 * sizeof(float),
+        (void*)0
+    );
+
+    glEnableVertexAttribArray(0);
+
+while (!glfwWindowShouldClose(window))
+{
+    glClearColor(0.2f, 0.4f, 0.8f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+
+    glDrawArrays(GL_LINES, 0, 8);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
 
     glfwDestroyWindow(window);
     glfwTerminate();
